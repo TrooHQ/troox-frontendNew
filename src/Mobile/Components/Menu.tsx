fbc842f1e527c0c3cbdc0265af3e7ad023f5a370
KEM-CONSOLATION <consolationlotachi@gmail.com>
2024-04-11 14:34:47 +0100

    fixes: removed unused variables

--- src/Mobile/Components/Menu.tsx
+++ src/Mobile/Components/Menu.tsx
@@ -1,11 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import Arrow from "../assets/BackArrow.svg";
-import DashboardBackButton from "../Buttons/DashboardBackButton.tsx";
 import MenuTab from "./MenuTab";
 
 const Menu = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="my-[16px] mx-[24px]">
-      <DashboardBackButton text="Menu" img={Arrow} />
+      <div
+        onClick={() => navigate(-1)}
+        className=" inline-flex items-center gap-[20px] cursor-pointer"
+      >
+        <img src={Arrow} alt="" />
+        <p className=" font-[500] text-[20px] text-grey500 cursor-pointer">
+          Menu
+        </p>
+      </div>
       <div className=" mt-[24px]">
         <MenuTab />
       </div>
