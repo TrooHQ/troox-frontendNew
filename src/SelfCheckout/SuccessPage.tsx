8adc32bae1e22c4dc5eb446043f501f3ab479527
KEM-CONSOLATION <consolationlotachi@gmail.com>
2024-10-13 22:38:33 +0100

    worked on the collection number

--- src/SelfCheckout/SuccessPage.tsx
+++ src/SelfCheckout/SuccessPage.tsx
@@ -1,9 +1,10 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { FaCircleCheck } from "react-icons/fa6";
+import { useEffect } from "react";
 
 const SuccessPage = () => {
-  const orderId = sessionStorage.getItem("orderId");
+  const collection_number = sessionStorage.getItem("collection_number");
 
   const url = sessionStorage.getItem("url");
 
@@ -11,31 +12,24 @@ const SuccessPage = () => {
     (state: RootState) => state.business?.businessDetails?.colour_scheme
   );
 
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = `${url}`;
+    }, 10000);
+  }, []);
+
   return (
     <div className="">
-      <div className=" text-black mt-[100px] max-w-[600px] mx-auto space-y-[16px] text-center">
+      <div className=" text-black mt-[100px] max-w-[600px] mx-auto space-y-[16px] text-start">
         <div className="flex items-center justify-center relative mb-[50px]">
           <div className="absolute inset-0 flex items-center justify-center">
             <div className="w-[150px] h-[150px] rounded-full animate-ping border-2 border-green-500"></div>
           </div>
           <FaCircleCheck className="text-[100px] relative z-10 text-green-500" />
         </div>
-
         <p className="text-[30px] font-[500]">Thank you!</p>
-        <p className="text-[30px] font-[500]">
-          Please, Kindly Find your{" "}
-          <span
-            className=" font-bold"
-            style={{
-              color: color,
-            }}
-          >
-            Collection Number
-          </span>{" "}
-          below and Present it at collection point
-        </p>
 
-        <div className="">
+        <div className=" space-y-[50px]">
           <p className="text-[30px] font-[500]">
             Your Collection Number is:
             <span
@@ -45,28 +39,11 @@ const SuccessPage = () => {
               }}
             >
               {" "}
-              {orderId}
+              {collection_number}
             </span>
           </p>
-        </div>
-      </div>
-
-      <div
-        className=" mt-[100px] flex items-center justify-center "
-        onClick={() => {
-          setTimeout(() => {
-            window.location.href = `${url}`;
-          }, 1000);
-        }}
-      >
-        <div
-          className="text-white text-center  rounded-[8px]"
-          style={{
-            backgroundColor: color,
-          }}
-        >
-          <p className="text-[40px] font-[500] px-[10px] py-[10px] ">
-            Continue
+          <p className="text-[30px] font-[500]">
+            Kindly present it at the collection point
           </p>
         </div>
       </div>
