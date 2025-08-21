e7959f124633f1275cd0e95166736f4ced4470f9
Element27 <olasocials@gmail.com>
2025-08-21 15:55:59 +0100

    updated

--- src/components/Dashboard/components/ModifierModal.tsx
+++ src/components/Dashboard/components/ModifierModal.tsx
@@ -6,7 +6,10 @@ const ModifierModal = ({
   handleConfirmSave,
   modifiers,
   setModifiers,
+  editModifierData,
+  setEditModifierData,
   loading,
+  handleUpdateMod,
 }: any) => {
   const updateModifier = (id: number, field: string, value: string) => {
     setModifiers(
@@ -15,6 +18,16 @@ const ModifierModal = ({
       )
     );
   };
+
+  const editModifier = (key: any, value: any) => {
+    setEditModifierData((prevModifier: any) => ({
+      ...prevModifier,
+      [key]: value,
+    }));
+  };
+
+  console.log('modifiers', modifiers)
+  console.log('editModifierData', editModifierData)
 
   return (
     <Modal isOpen={addModifierModar} onClose={() => setAddModifierModal(false)}>
@@ -30,54 +43,65 @@ const ModifierModal = ({
                 id: any;
                 price: any;
                 name: any;
+
               },
               index: number
             ) => (
               <div key={index} className="flex items-center gap-[8px] justify-center mb-[8px]">
                 {/* <img src={AddWhite} alt="" onClick={handleAddModifier} className="cursor-pointer" /> */}
-                <div className="flex items-center gap-[8px]">
-                  <input
-                    type="text"
-                    className="border border-[#929292] rounded-[5px] placeholder:text-[#929292] py-[12px] px-[8px] w-[300px]"
-                    placeholder=" Enter modifier name "
-                    value={modifier.name}
-                    onChange={(e) => updateModifier(modifier.id as any, "name", e.target.value)}
-                  />
-                  <input
-                    type="text"
-                    className="border border-[#929292] rounded-[5px] placeholder:text-[#929292] py-[12px] px-[8px] w-[100px]"
-                    placeholder=" Enter price "
-                    value={modifier.price}
-                    onChange={(e) => updateModifier(modifier.id, "price", e.target.value)}
-                  />
-                  {/* {index > 0 && (
-                    <img
-                      src={RemoveIcon}
-                      alt="Remove"
-                      onClick={() => handleRemoveModifier(modifier.id)}
-                      className="cursor-pointer"
+                {editModifierData ?
+                  <div className="flex items-center gap-[8px]">
+                    <input
+                      type="text"
+                      className="border border-[#929292] rounded-[5px] placeholder:text-[#929292] py-[12px] px-[8px] w-[300px]"
+                      placeholder=" Enter modifier name "
+                      value={editModifierData.modifier_name}
+                      onChange={(e) => editModifier("modifier_name", e.target.value)}
                     />
-                  )} */}
-                </div>
+                    <input
+                      type="text"
+                      className="border border-[#929292] rounded-[5px] placeholder:text-[#929292] py-[12px] px-[8px] w-[100px]"
+                      placeholder=" Enter price "
+                      value={editModifierData.modifier_price}
+                      onChange={(e) => editModifier("modifier_price", e.target.value)}
+                    />
+                  </div> :
+                  <div className="flex items-center gap-[8px]">
+                    <input
+                      type="text"
+                      className="border border-[#929292] rounded-[5px] placeholder:text-[#929292] py-[12px] px-[8px] w-[300px]"
+                      placeholder=" Enter modifier name "
+                      value={modifier.name}
+                      onChange={(e) => updateModifier(modifier.id as any, "name", e.target.value)}
+                    />
+                    <input
+                      type="text"
+                      className="border border-[#929292] rounded-[5px] placeholder:text-[#929292] py-[12px] px-[8px] w-[100px]"
+                      placeholder=" Enter price "
+                      value={modifier.price}
+                      onChange={(e) => updateModifier(modifier.id, "price", e.target.value)}
+                    />
+                  </div>
+                }
               </div>
             )
           )}
 
           <hr className="border mb-[16px] mt-[24px] border-[#E7E7E7]" />
 
-          <div className="flex justify-end items-center gap-2">
+          <div className="flex items-center justify-end gap-2">
             <div
               className="border cursor-pointer border-purple500 rounded px-[24px] py-[10px] font-[600] text-purple500"
-              onClick={() => setAddModifierModal(false)}
+              onClick={() => { setEditModifierData(null); setAddModifierModal(false); setModifiers([]) }}
             >
               <p className="font-[500] text-[16px] text-purple500 cursor-pointer">Cancel</p>
             </div>
 
             <div
               className="border border-purple500 bg-purple500 rounded px-[24px] py-[10px] font-[500] text-[#ffffff]"
-              onClick={() => handleConfirmSave(modifiers)}
+              onClick={() => { editModifierData ? handleUpdateMod() : handleConfirmSave(modifiers) }}
             >
-              <button className=" text-[16px]">{loading ? "Saving..." : "Save item"}</button>
+              {editModifierData ? <button className=" text-[16px]">Update item</button> : <button className=" text-[16px]">{loading ? "Saving..." : "Save item"}</button>}
             </div>
           </div>
         </div>
