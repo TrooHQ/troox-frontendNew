1c5d5d3482e6e059b283c84c8bf47f3b2e691ed9
Element27 <olasocials@gmail.com>
2026-02-01 23:35:32 +0100

    menu ui update

--- src/components/Dashboard/NewMenuBuilder/components/DataTable.tsx
+++ src/components/Dashboard/NewMenuBuilder/components/DataTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { MenuItemProp } from '../types';
 
 
--- src/components/Dashboard/TableQRCode.tsx
+++ src/components/Dashboard/TableQRCode.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
-import { Box, Typography, IconButton, Button, Modal } from "@mui/material";
-import { Close, Add, DeleteOutline } from "@mui/icons-material";
+// import React, { useState } from "react";
+// import { Box, Typography, IconButton, Button, Modal } from "@mui/material";
+// import { Close, Add, DeleteOutline } from "@mui/icons-material";
+
+import { Box, Typography,  Button,  } from "@mui/material";
+import {  Add,  } from "@mui/icons-material";
 
 interface TableQRCodeProps {
   onOpenEditModal: () => void;
